1ecf9e0ef764768223e2b06b729f0f4873565723
Lukita <lucmaieski@gmail.com>
2022-08-16 18:44:46 -0300

    Add secret

--- dist/app/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const userMiddlewares_1 = require("../middlewares/userMiddlewares");
-const userRouter = (0, express_1.Router)();
-userRouter.post('/create', userMiddlewares_1.emailValidation, userMiddlewares_1.nameValidation, userMiddlewares_1.passwordValidation, userController_1.createUser);
-userRouter.post('/login', userMiddlewares_1.emailValidation, userMiddlewares_1.passwordValidation, userController_1.loginUser);
-exports.default = userRouter;
