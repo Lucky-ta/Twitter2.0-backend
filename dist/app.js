f1a0a51f802b79f89d4370bc470172b3bd422aa2
Lukita <lucmaieski@gmail.com>
2022-08-17 15:21:49 -0300

    Add cors

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const tweetRouter_1 = __importDefault(require("./app/routes/tweetRouter"));
 const userRouter_1 = __importDefault(require("./app/routes/userRouter"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use('/user', userRouter_1.default);
 app.use('/tweet', tweetRouter_1.default);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 import tweetRouter from './app/routes/tweetRouter';
 import userRouter from './app/routes/userRouter';
 
 const app: Express = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/user', userRouter);
 app.use('/tweet', tweetRouter);
