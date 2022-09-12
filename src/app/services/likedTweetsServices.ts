898450e2d51dda90ddf46ce2852a1b8d5c8ea03e
Lukita <lucmaieski@gmail.com>
2022-09-12 17:57:57 -0300

    Create association between tweet and likedTweets tables

--- src/app/services/likedTweetsServices.ts
+++ src/app/services/likedTweetsServices.ts
@@ -1,4 +1,4 @@
-const { LikedTweets } = require('../../database/models');
+const { LikedTweets, Tweet } = require('../../database/models');
 
 const likeNewTweet = async (userId: number, tweetId: number) => {
   const likeTweet = await LikedTweets.create({ userId, tweetId });
@@ -17,11 +17,16 @@ const deslikeNewTweet = async (userId: number, tweetId: number) => {
 };
 
 const listLikedTweets = async (userId: number) => {
-  const likedTweets = await LikedTweets.findAll({ where: { userId } });
+  const likedTweets = await LikedTweets.findOne({
+    where: { userId },
+    include: [
+      { model: Tweet, required: true, attributes: ['tweet'] },
+    ],
+  });
 
   if (likedTweets !== null) {
     return { status: 201, data: likedTweets };
-  } return { status: 404, data: { message: 'User not found' } };
+  } return { status: 404, data: { message: 'Tweet not found' } };
 };
 
 export { likeNewTweet, deslikeNewTweet, listLikedTweets };
