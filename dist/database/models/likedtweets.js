cb84494a78c57c88473a9f8096d5999b774011c8
Lukita <lucmaieski@gmail.com>
2022-09-12 15:25:36 -0300

    Generate dist file

--- dist/database/models/likedtweets.js
+++ dist/database/models/likedtweets.js
@@ -1,7 +1,7 @@
 "use strict";
-const { Model } = require('sequelize');
+const { Model, } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class User extends Model {
+    class likedTweets extends Model {
         /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
@@ -9,19 +9,14 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
-            User.hasMany(models.Tweet, {
-                foreignKey: 'userId',
-            });
         }
     }
-    User.init({
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        likedTweets: DataTypes.INTEGER,
+    likedTweets.init({
+        userId: DataTypes.INTEGER,
+        tweetId: DataTypes.INTEGER,
     }, {
         sequelize,
-        modelName: 'User',
+        modelName: 'LikedTweets',
     });
-    return User;
+    return likedTweets;
 };
