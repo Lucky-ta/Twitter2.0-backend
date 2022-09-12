9dce1194cb843f4ec30e85ac0ba13c166a83b513
Lukita <lucmaieski@gmail.com>
2022-09-12 15:24:59 -0300

    Add tokenValidation for likedTweets routes

--- src/app/routes/likedTweets.ts
+++ src/app/routes/likedTweets.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
 import { addLikedTweet, RemoveLikedTweet } from '../controllers/likedTweetsController';
+import { tokenValidation } from '../middlewares/tweetMiddlewares';
 
 const likedTweetsRouter = Router();
 
-likedTweetsRouter.post('/:userId/:tweetId', addLikedTweet);
-likedTweetsRouter.delete('/:userId/:tweetId', RemoveLikedTweet);
+likedTweetsRouter.post(
+  '/:userId/:tweetId',
+  tokenValidation,
+
+  addLikedTweet,
+);
+
+likedTweetsRouter.delete(
+  '/:userId/:tweetId',
+  tokenValidation,
+
+  RemoveLikedTweet,
+);
 
 export default likedTweetsRouter;
