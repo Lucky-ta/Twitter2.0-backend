e2d2adf5a4444d2039e51f2a7890f1021d90f6de
Lukita <lucmaieski@gmail.com>
2022-09-23 17:15:47 -0300

    Remove dist file for github

--- dist/__tests__/utils/truncateDb.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const { sequelize } = require('../../src/database/models');
-module.exports = () => Promise.all(Object.keys(sequelize.models)
-    .map((key) => sequelize.models[key].destroy({ truncate: true, force: true })));
--- dist/src/app/services/errorMessages/tweetMessages.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tweetErrors = {
-    tweetError: 'Tweet not found',
-};
-exports.default = tweetErrors;
