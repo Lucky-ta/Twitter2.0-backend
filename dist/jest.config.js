7462b3c3e5d01bf812cfefc40f41412afe7ccc76
Lukita <lucmaieski@gmail.com>
2022-09-26 17:15:02 -0300

    Generate dist file

---
+++ dist/jest.config.js
@@ -0,0 +1,9 @@
+"use strict";
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    clearMocks: true,
+    bail: false,
+    testMatch: ['**/__tests__/**/*.test.ts?(x)'],
+};
