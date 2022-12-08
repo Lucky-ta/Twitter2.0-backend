d008ef9626c2ca6ed5e6328df44335d0593df44f
Lukita <lucmaieski@gmail.com>
2022-12-08 17:56:07 -0300

    Remove dist file

--- dist/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    clearMocks: true,
-    bail: false,
-    testMatch: ['**/__tests__/**/*.test.ts?(x)'],
-};
--- dist/src/app/middlewares/errorMessages/tweetErrorMessages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tweetErrors = {
-    tweetError: 'Invalid tweet format',
-    signError: 'Invalid sign format',
-};
-exports.default = tweetErrors;
