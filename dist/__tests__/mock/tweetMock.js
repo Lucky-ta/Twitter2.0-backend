23f2f4e7587fce8c90afa3b290ef31afed96898d
Lukita <lucmaieski@gmail.com>
2022-12-08 21:06:42 -0300

    Remove dist file

--- dist/__tests__/mock/tweetMock.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tweetMock = {
-    validTweet: 'There here is a valid new tweet',
-    invalidTweet: '',
-};
-exports.default = tweetMock;
